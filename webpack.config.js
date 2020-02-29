b90ecfd4654030a7805e74c85f746f8797c9a750
Alexander Lomonosov <skilldill@yandex.ru>
2020-02-29 22:21:57 +0500

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require("path");
 const HTMLPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    entry: "./src/main.js",
+    entry: "./src/js/main.js",
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.[chunkhash].js'
@@ -10,9 +12,26 @@ module.exports = {
     devServer: {
         port: 3000
     },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader',
+                ],
+            },
+        ],
+    },
     plugins: [
         new HTMLPlugin({
             template: "./src/index.html"
-        })
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
+        new CleanWebpackPlugin()
     ]
 }
